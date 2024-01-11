b95ffdcf4516f1c3ef99e44ecd9da7dc5bbb774e
just-toni <75427368+just-toni@users.noreply.github.com>
2024-01-11 18:39:36 -0400

    completed level 3 exercises for day 1

---
+++ dayOne/level3.py
@@ -0,0 +1,22 @@
+# 1
+typeInt = 1
+typeFloat = 1.5
+typeComplex = 3+4j
+
+typeString = "pool"
+typeBoolean = True
+typeList = [1, 2, 3, 4, 5]
+typeTuple = ('tee', 'bee', 'see')
+typeSet = {1, 2, 3, 4, 6}
+typeDictionary = {'name': 'randy', 'type': 'tiger', 'gender': 'male'}
+
+# 2 - euclidean distance
+# two dimension
+p1 = 2
+p2 = 3
+q1 = 10
+q2 = 8
+
+answer = ((q1 - p1) ** 2) + ((q2 - p2) ** 2)
+print(answer)
+# final answer would be the square root of the answer variable which is 9
