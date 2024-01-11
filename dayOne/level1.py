28869f1d863cf81e202f202b9acef3ea5a69277e
just-toni <75427368+just-toni@users.noreply.github.com>
2024-01-11 18:25:41 -0400

    completed level 1 exercises for day 1

---
+++ dayOne/level1.py
@@ -0,0 +1,24 @@
+# 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# 3
+name = "toni"
+familyname = 'Adeniyi'
+country = "Canada"
+word = "I am enjoying 30 days of python"
+
+# 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Tee', 'Python', 'Canada']))
+print(type(name))
+print(type(familyname))
+print(type(country))
