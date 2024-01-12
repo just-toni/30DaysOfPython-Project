ee0e23c5342519fb3d80068d07a7a3aeba59f401
just-toni <75427368+just-toni@users.noreply.github.com>
2024-01-12 16:25:53 -0400

    completed all level 1 exercises for day 2

---
+++ dayTwo/level1.py
@@ -0,0 +1,35 @@
+# 2
+# Day 2: 30 Days of Python programming
+
+# 3
+first_name = 'Toni'
+
+# 4
+last_name = 'Adeniyi'
+
+# 5
+full_name = first_name + last_name
+
+# 6
+country = 'Canada'
+
+# 7
+city = 'Halifax'
+
+# 8
+age = 20
+
+# 9
+year = 2024
+
+# 10
+is_married = False
+
+# 11
+is_true = True
+
+# 12
+is_light_on = False
+
+# 13
+food, drink, color = 'Fried plantains', 'Oreo milkshakes', 'beige'
