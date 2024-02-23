649b5cd934e6d2d70a4f6c71e5adcfa1e021843b
just-toni <75427368+just-toni@users.noreply.github.com>
2024-02-22 22:53:26 -0400

    level 3 of day 7 completed

--- daySeven/level1.py
+++ daySeven/level1.py
@@ -1,8 +1,5 @@
 # sets
 it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
-A = {19, 22, 24, 20, 25, 26}
-B = {19, 22, 20, 25, 26, 24, 28, 27}
-age = [22, 19, 24, 25, 26, 24, 25, 24]
 
 # 1
 print(len(it_companies))
